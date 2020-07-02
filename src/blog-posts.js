f199d164838dca68c94912ddd13ca0ab4cf2d458
CodeTeen <63883456+CodeTeen@users.noreply.github.com>
2020-07-03 01:47:10 +0300

    Update blog-posts.js

--- src/blog-posts.js
+++ src/blog-posts.js
@@ -4,6 +4,7 @@ export const getPosts = () => {
       writer: "Fahri Muhammet Demir",
       title: "1500TL ödül! Sen de yarışmaya katıl!",
       slug: "yarisma",
+      intro:"Sitemin Post Kısmı Böyle Görünecek",
       details: require("./posts/yarisma.md").default,
       date: "5 Aralık 2019",
       img: "https://picsum.photos/600"
@@ -12,6 +13,7 @@ export const getPosts = () => {
       writer: "Fahri Muhammet Demir",
       title: "Örnek yazı",
       slug: "ornek-yazi",
+      intro:"Sitemin Post Kısmı Böyle Görünecek",
       details: require("./posts/ornek-yazi.md").default,
       date: "3 Aralık 2019",
       img: "https://picsum.photos/600"
@@ -20,6 +22,7 @@ export const getPosts = () => {
       writer: "Fahri Muhammet Demir",
       title: "Merhaba dünya!",
       slug: "merhaba",
+      intro:"Sitemin Post Kısmı Böyle Görünecek",
       details: require("./posts/merhaba.md").default,
       date: "1 Aralık 2019",
       img: "https://picsum.photos/600"
